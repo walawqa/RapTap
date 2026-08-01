f979e4bf47b5aef65cb341248377ff749be2f8c8
walawqa <walawka.sub@gmail.com>
2026-08-01 13:13:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'forma-zycia-steel-v3';
+const CACHE = 'forma-zycia-steel-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
